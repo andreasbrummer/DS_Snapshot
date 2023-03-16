b32c2638d0f7b618779f4e77af8effce14097353
andreasbrummer <andreas.brummer00@gmail.com>
2023-03-16 22:18:33 +0100

    other improvements

--- src/SenderMessageTest.java
+++ src/SenderMessageTest.java
@@ -1,12 +1,10 @@
 import SnapshotLibrary.DistributedSnapshot;
-import SnapshotLibrary.Messages.Marker;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.InetAddress;
-import java.util.LinkedHashMap;
-import java.util.UUID;
+import java.util.logging.Logger;
 
 import static java.lang.Thread.sleep;
 
@@ -14,21 +12,27 @@ public class SenderMessageTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         int port1=24071,port2=24079;
         DistributedSnapshot ds = new DistributedSnapshot();
-       int port = 24072; //porta d ascolto
+        //listener port
+       int port = 24072;
             if (args.length > 0) {
                 if(Integer.parseInt(args[0])==0){
-                    ds.init(port1);
-                    sleep(10000);
-                    port=port2;
+                    while(!ds.init(port1)){
+                        sleep(5000);
+                        Logger.getLogger("SenderMessageTest").info("Porta "+port1 + " occupata, aspetto 5 secondi");
+                    }
+                    port = port2;
+                    sleep(5000);
                 }
                 else {
-                    sleep(1000);
-                    ds.init(port2);
+                    while (!ds.init(port2)) {
+                        sleep(1000);
+                        Logger.getLogger("SenderMessageTest").info("Porta " + port2 + "occupata, aspetto 5 secondi");
+                    }
                     port = port1;
                 }
             }
             else{
-                System.out.println("Inserire 0 o 1 come argomento\n");
+                System.out.println("Insert 0 o 1 as argument\n");
             }
 
 
@@ -36,17 +40,20 @@ public class SenderMessageTest {
         //harcodare qui la porta del nodo server 10720
         String node1 = ds.installNewConnectionToNode(ipAddress, port);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        while (true) {
+        Object object = reader.readLine();
+        while (!object.equals("fine")) {
                 System.out.print("Inserisci l'oggetto da inviare: \n");
                 // Qui si assume che l'oggetto sia di tipo String, ma è possibile utilizzare qualsiasi altro tipo di oggetto
-                Object object = reader.readLine();
                 if(object.equals("marker")){
                     ds.startSnapshot();
                 }
                 else{
                     ds.sendMessage(node1,object);
                 }
+                object = reader.readLine();
       }
+        System.out.println("Chiusura del programma");
+        ds.end();
     }
 }
 //        try {
--- src/SnapshotLibrary/DistributedSnapshot.java
+++ src/SnapshotLibrary/DistributedSnapshot.java
@@ -1,7 +1,6 @@
 package SnapshotLibrary;
 
 import SnapshotLibrary.Messages.Marker;
-import SnapshotLibrary.Messages.SnapMsg;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -16,11 +15,14 @@ import java.util.*;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-import static java.lang.Thread.sleep;
 
 
-    /*
+
+
+/*
     * @author: Andreas Brummer
     * @author: Francesco Caserta
     * @date: 2023-03-16
@@ -42,14 +44,14 @@ import static java.lang.Thread.sleep;
     * */
 
 public class DistributedSnapshot{
-    private int serverPortNumber;
     private Serializable status;
     private Server server;
-    private Path path;
-    private List<SocketAddress> input_nodes = new ArrayList<SocketAddress>();
-    private Map<UUID,Snapshot> snapshots = new HashMap<>();
-    Map<UUID,Socket> output_nodes = new HashMap<>();
-    Map<UUID,ObjectOutputStream> output_stream = new HashMap<>();
+    private final Path path;
+    private final List<SocketAddress> input_nodes = new ArrayList<>();
+    private final Map<UUID,Snapshot> snapshots = new HashMap<>();
+    private final Map<UUID,Socket> output_nodes = new HashMap<>();
+    private final Map<UUID,ObjectOutputStream> output_stream = new HashMap<>();
+    private static final Log LOGGER = LogFactory.getLog(DistributedSnapshot.class);
 
 
     /*  only for testing
@@ -57,8 +59,8 @@ public class DistributedSnapshot{
     private static final int SNAPSHOT_START_DELAY_MS = 10000;
 
     /*  only for testing */
-    private static boolean TEST_MODE = true;
-
+    private static final boolean TEST_MODE = true;
+    //TODO capire il discorso delle cartella e dei file (es. se cartella è gia esistente)
     public DistributedSnapshot(Path path) {
         this.path = path;
     }
@@ -68,16 +70,20 @@ public class DistributedSnapshot{
     public DistributedSnapshot() {
         this.path = Storage.createFolder("Snapshots");
     }
+    //TODO: pensare se mettere come argomento del costruttore anche lo status
 
-    public void init(int serverPortNumber) throws IOException {
-        server = new Server();
-        server.start(serverPortNumber);
-        System.out.println("Server started.");
+
+    public boolean init(int serverPortNumber) {
+            server = new Server();
+            server.start(serverPortNumber);
+            LOGGER.info("Server started.");
+            return true;
     }
 
-    public void end() { // close server
+
+    public void end() throws IOException { // close server
         server.stop();
-        System.out.println("Server stopped.");
+        LOGGER.info("Server stopped.");
     }
 
     public String installNewConnectionToNode(InetAddress ip, int port) throws IOException {
@@ -90,7 +96,6 @@ public class DistributedSnapshot{
     }
 
     public void sendMessage(String node_id, Object msg) throws IOException {
-        Socket destination = output_nodes.get(UUID.fromString(node_id));
         output_stream.get(UUID.fromString(node_id)).writeObject(msg);
     }
 
@@ -104,12 +109,16 @@ public class DistributedSnapshot{
         }
     }
 
-    public void endSnapshot(Snapshot snapshot) throws IOException {
-        System.out.println("Snapshot " + snapshot.getSnapshotId() + " ended.");
+    public void endSnapshot(Snapshot snapshot)  {
+        LOGGER.info("Snapshot " + snapshot.getSnapshotId() + " ended.");
         Storage.storeSnapshot(snapshot, path);
         snapshots.remove(snapshot.getSnapshotId());
         // test
-        snapshot.print_snapshot();
+        if(TEST_MODE) {
+            LOGGER.debug("Printing snapshot... " );
+            LOGGER.debug(snapshot.toString());
+        }
+
 
     }
 
@@ -130,8 +139,7 @@ public class DistributedSnapshot{
             try {
                 serverSocket = new ServerSocket(portNumber);
                 int port = serverSocket.getLocalPort();
-                serverPortNumber = port;
-                System.out.println("Server started on port" + port);
+                LOGGER.info("Server started on port" + port);
                 running = true;
                 serverThread = new Thread(() -> {
                     while (running) {
@@ -139,7 +147,7 @@ public class DistributedSnapshot{
                             Socket socket = serverSocket.accept();
                             input_nodes.add(socket.getRemoteSocketAddress());
                             // Gestisci la connessione del client qui:
-                            System.out.println("New connection established with:" + socket.getInetAddress() + " port:" + socket.getPort());
+                            LOGGER.info("New connection established with:" + socket.getInetAddress() + " port:" + socket.getPort());
                             new Thread(new NodeHandler(socket)).start();
 
                         } catch (IOException e) {
@@ -153,15 +161,23 @@ public class DistributedSnapshot{
             }
         }
 
-        public void stop() {
+        public void stop() throws IOException {
             try {
                 running = false;
                 serverSocket.close();
                 serverThread.join();
-            } catch (IOException | InterruptedException e) {
-                e.printStackTrace();
+                LOGGER.info("Server stopped.");
+            } catch (IOException e) {
+                if(!serverSocket.isClosed())
+                    serverSocket.close();
+                LOGGER.error("Error closing server socket", e);
+            } catch (InterruptedException e) {
+                LOGGER.error("Interrupted while waiting for server thread to join", e);
+                // Restore interrupted status
+                Thread.currentThread().interrupt();
             }
         }
+
     }
 
 
@@ -169,7 +185,7 @@ public class DistributedSnapshot{
 
 
     private class NodeHandler implements Runnable {
-        private Socket clientSocket;
+        private final Socket clientSocket;
 
         public NodeHandler(Socket socket) {
             this.clientSocket = socket;
@@ -185,12 +201,9 @@ public class DistributedSnapshot{
             if (snapshots.containsKey(snapshotId)) {
                 // Case: snapshot in progress
                 snapshot = snapshots.get(snapshotId);
-                if (snapshot.remove_from_node_address_list(clientSocket.getRemoteSocketAddress()))
-                    // If it was the last marker, end the snapshot
-                    endSnapshot(snapshot);
             } else {
                 // Case: starting snapshot
-                System.out.println("Starting snapshot " + snapshotId);
+                LOGGER.info("Starting snapshot " + snapshotId);
                 snapshot = new Snapshot(snapshotId, status, input_nodes);
 
                 snapshots.put(snapshotId, snapshot);
@@ -219,10 +232,10 @@ public class DistributedSnapshot{
                         }
                     });
                 }
-                if (snapshot.remove_from_node_address_list(clientSocket.getRemoteSocketAddress()))
-                    // If it was the last marker, end the snapshot
-                    endSnapshot(snapshot);
             }
+            // If it was the last marker, end the snapshot
+            if (snapshot.removeFromNodeAddressList(clientSocket.getRemoteSocketAddress()))
+                endSnapshot(snapshot);
         }
 
 
@@ -234,13 +247,13 @@ public class DistributedSnapshot{
         private void handleMessage(Object message) {
             if (snapshots.isEmpty()) {
                 // No snapshot in progress: do not save received messages
-                System.out.println("Not saving received messages");
+                LOGGER.debug("Not saving received messages");
             } else {
                 // Snapshot in progress: save received messages
                 for (Snapshot snapshot : snapshots.values()) {
-                    System.out.println("Saving received messages: "+ message);
-                    if (snapshot.getConnected_nodes().contains(clientSocket.getRemoteSocketAddress())) {
-                        snapshot.addNode_messages_List(clientSocket.getRemoteSocketAddress(), message);
+                    LOGGER.debug("Saving received messages: "+ message);
+                    if (snapshot.getConnectedNodes().contains(clientSocket.getRemoteSocketAddress())) {
+                        snapshot.addNodeMessage(clientSocket.getRemoteSocketAddress(), message);
                     }
                 }
             }
@@ -255,26 +268,38 @@ public class DistributedSnapshot{
                 while ((inputObject = in.readObject()) != null) {
                     /*Controllo se ho ricevuto un marker*/
                     if (inputObject instanceof Marker ) {
-                        System.out.println("Received a new marker:\n Id: " + ((Marker) inputObject).getSnapshotId());
+                        LOGGER.debug("Received a new marker:\n Id: " + ((Marker) inputObject).getSnapshotId());
                         handleMarker((Marker) inputObject);
                     } else {
-                        System.out.println("Received a new message: " + inputObject);
+                        LOGGER.debug("Received a new message: " + inputObject);
                         handleMessage(inputObject);
                     }
                 }
             } catch (IOException e) {
-                System.out.println("chiuso il socket");
+                if (clientSocket.isClosed()) {
+                    // Socket closed by remote host
+                    LOGGER.error("Socket chiuso dal lato remoto.");
+
+                    //TODO: togliere da input_nodes e chiudere questo thread.
+                } else {
+                    // Other I/O errors
+                    e.printStackTrace();
+                }
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            } catch (ClassNotFoundException e ) {
-                throw new RuntimeException(e);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+            } finally {
+                // Socket closing
+                try {
+                    clientSocket.close(); //IN TEORIA NON DOBBIAMO CHIUDERE NOI IL SOCKET DEL CLIENT. DEVE CHIUDERLO IL CLIENT
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
             }
         }
 
     }
-}
-
-
 
 
